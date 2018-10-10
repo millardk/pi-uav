799ecd017dbb05384679f4b6504f202eaf56aa9f
Keegan Millard <keegan@Keegans-MacBook-Pro.local>
2018-10-09 18:39:41 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CS370_termproject
---
+++ server.py
@@ -0,0 +1,29 @@
+import http.server
+import socketserver
+
+import RPi.GPIO as GPIO
+
+HOST = ""
+PORT = 8081
+
+class MyHTTPHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/LED_ON":
+            GPIO.output(19, GPIO.HIGH)
+            self.send_response(200)
+
+        elif self.path == "/LED_OFF":
+            GPIO.output(19, GPIO.LOW)
+            self.send_response(200)
+
+        else:
+            self.send_response(404)
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(19,GPIO.OUT)
+
+server = socketserver.TCPServer((HOST,PORT), MyHTTPHandler)
+server.serve_forever()
