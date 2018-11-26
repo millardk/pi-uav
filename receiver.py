366a2582ba994e1f01620783f5834d5f1cba554d
keegan <keegan.millard@gmail.com>
2018-11-26 09:12:30 -0700

    connection led support

--- receiver.py
+++ receiver.py
@@ -1,10 +1,11 @@
 from digi.xbee.devices import XBeeDevice
 import Adafruit_PCA9685
-
+import pigpio 
 # TODO: Replace with the serial port where your local module is connected to. 
 
 servo_min = 205
 servo_max = 410
+ledStatus=True
 
 def map_value(val):
     range = servo_max - servo_min
@@ -12,27 +13,32 @@ def map_value(val):
     return int(servo_min + val*inc)
 
 def main():
-    print(" +-----------------------------------------+")
-    print(" | XBee Python Library Receive Data Sample |")
-    print(" +-----------------------------------------+\n")
-
-    device = XBeeDevice("/dev/ttyAMA0", 9600)
-    pwm = Adafruit_PCA9685.PCA9685()
-    pwm.set_pwm_freq(50)
-    
-    last = bytearray([0,0,0,0])
-    
     def set_servo(channel, setting):
                 output = map_value(setting)
                 pwm.set_pwm(channel, 0, output)
                 
-    for channel in range(4):
-        set_servo(channel, last[channel])
-
-    try:
+    try:    
+        device = XBeeDevice("/dev/ttyAMA0", 9600)
         device.open()
         
+        pwm = Adafruit_PCA9685.PCA9685()
+        pwm.set_pwm_freq(50)
+        
+        pi= pigpio.pi()
+        pi.write(16,1)
+       
+        last = bytearray([0,0,0,0])
+        for channel in range(4):
+            set_servo(channel, last[channel])
+        
         def data_receive_callback(xbee_message):
+            global ledStatus
+            if ledStatus:
+                pi.write(16,0)
+                ledStatus = False
+            else:
+                pi.write(16,1)
+                ledStatus = True
 ##            print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
 ##                                     xbee_message.data))
             for channel in range(4):
