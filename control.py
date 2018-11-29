a6818ca8cf72c98f4f7eefa343238ce6b832f62d
Keegan <keegan.millard@gmail.com>
2018-11-28 22:12:32 -0700

    added flaps and autopilot controls

--- control.py
+++ control.py
@@ -15,9 +15,16 @@ class ControlState:
         self.throttle = 0
         self.rudder = 128
 
+        self.dec_flaps_pressed = False
+        self.inc_flaps_pressed = False
+        self.ap_mode_pressed = False
+
+        self.flaps = 0
+        self.ap_mode = 0
+
     def __repr__(self):
         return str(self.aileron) + " " + str(self.elevator) \
-               + " " + str(self.throttle) + " " + str(self.rudder)
+               + " " + str(self.throttle) + " " + str(self.rudder) + " "+ str(self.flaps) + " " + str(self.ap_mode)
 
     def update(self):
         events = get_gamepad()
@@ -28,27 +35,52 @@ class ControlState:
                     temp = scale_16_to_8(event.state) + 128
                     if (temp < self.aileron - diff) | (temp > self.aileron + diff):
                         self.aileron = temp
-                    # self.aileron = scale_16_to_8(event.state) + 128
 
                 elif event.code is "ABS_RY":
                     temp = scale_16_to_8(event.state) + 128
                     if (temp < self.elevator - diff) | (temp > self.elevator + diff):
                         self.elevator = temp
-                    # self.elevator = scale_16_to_8(event.state) + 128
 
                 elif event.code is "ABS_Z":
                     temp = event.state
                     if temp < 20:
                         self.throttle = 0
                     elif (temp < self.throttle - diff) | (temp > self.throttle + diff):
-                       self.throttle = temp
-                    # self.throttle = event.state
+                        self.throttle = temp
 
                 elif event.code is "ABS_X":
                     temp = scale_16_to_8(event.state) + 128
                     if (temp < self.rudder - diff) | (temp > self.rudder + diff):
                         self.rudder = temp
-                    # self.rudder = scale_16_to_8(event.state) + 128
+
+            elif event.ev_type is "Key":
+                # Buttons send an event when pressed and released
+                # Made it so every other event will toggle the value
+                if event.code is "BTN_TL":
+                    if self.dec_flaps_pressed is False:
+                        self.dec_flaps_pressed = True
+                        if self.flaps > 0:
+                            self.flaps -= 1
+                    else:
+                        self.dec_flaps_pressed = False
+
+                elif event.code is "BTN_TR":
+                    if self.inc_flaps_pressed is False:
+                        self.inc_flaps_pressed = True
+                        if self.flaps < 3:
+                            self.flaps += 1
+                    else:
+                        self.inc_flaps_pressed = False
+
+                elif event.code is "BTN_SOUTH":
+                    if self.ap_mode_pressed is False:
+                        self.ap_mode_pressed = True
+                        if self.ap_mode is 0:
+                            self.ap_mode = 1
+                        else:
+                            self.ap_mode = 0
+                    else:
+                        self.ap_mode_pressed = False
 
     def get_output(self):
         out = bytearray()
@@ -56,6 +88,7 @@ class ControlState:
         out.append(self.elevator)
         out.append(self.throttle)
         out.append(255-self.rudder)
+        out.append((self.flaps << 6) | (self.ap_mode << 5))
         return out
 
 
@@ -76,17 +109,15 @@ if __name__ == "__main__":
     last = cstate.get_output()
     last_time = int(round(time.time() * 1000))
 
+    # print(cstate)
+
     while 1:
         cstate.update()
         output = cstate.get_output()
-        # print(output)
-        # print(cstate)
         now = int(round(time.time() * 1000))
 
-        if ((output != last) & (now - last_time > 75)) | (last[2] != 0 and cstate.throttle == 0) | (abs(last[2] - cstate.throttle) > 10):
+        if ((output != last) & (now - last_time > 75)) | (last[2] != 0 and cstate.throttle == 0) | (abs(last[2] - cstate.throttle) > 10) | output[4] != last[4]:
             last = output
             last_time = now
-            # device.send_data(remote_device, output)
             device.send_data_async(remote_device, output)
-            print(cstate)
-        # sleep(1/15)
+            # print(cstate)
