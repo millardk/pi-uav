f2b59a2ba4c639b3ca6c2ba0dbbe63a4df91c57f
keegan <keegan.millard@gmail.com>
2018-11-28 23:50:40 -0700

    preliminary flaps support

--- receiver.py
+++ receiver.py
@@ -9,6 +9,7 @@ servo_min = 205
 servo_max = 410
 
 last_received = 0
+last_checked = 0
 
 def map_value(val):
     range = servo_max - servo_min
@@ -28,17 +29,31 @@ def main():
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(50)
        
-        last = bytearray([0,0,0,0])
-        for channel in range(4):
-            set_servo(channel, last[channel])
+        last = bytearray([0,0,0,0,0,0])
+##        for channel in range(6):
+##            set_servo(channel, last[channel])
         
         def data_receive_callback(xbee_message):
+            global last_received
             last_received = xbee_message.timestamp
             for channel in range(4):
                 if last[channel] != xbee_message.data[channel]:
                     set_servo(channel, xbee_message.data[channel])
                     last[channel] = xbee_message.data[channel]
-
+                    
+            flaps = xbee_message.data[4] >> 6 & 3;
+            print(flaps)
+            left_flap = 128-(flaps+1)*32
+            if last[4] != left_flap:
+                set_servo(4, left_flap)
+                last[4] = left_flap
+                
+            right_flap = (flaps+1)*45-1
+            if last[5] != right_flap:
+                set_servo(5, right_flap)
+                last[5] = right_flap
+            
+            ap_mode = xbee_message.data[4] >> 5 & 1;
 
         device.add_data_received_callback(data_receive_callback)
         print("Waiting for data...\n")
@@ -49,15 +64,18 @@ def main():
             device.close()
 
 
-def red_blink():
+def blink():
     pi = pigpio.pi()
     pi.set_mode(13, pigpio.OUTPUT)
     pi.set_mode(16, pigpio.OUTPUT)
+    
 
     def check_connection():
         global last_received
-        cur_time =  int(round(time.time() * 1000))
-        return  cur_time - last_received > 500
+        global last_checked
+        connection = last_received != last_checked
+        last_checked = last_received
+        return connection
 
     while(True):
         if check_connection():
@@ -70,6 +88,6 @@ def red_blink():
         time.sleep(1.5)
 
 if __name__ == '__main__':
-    t = threading.Thread(target=red_blink)
+    t = threading.Thread(target=blink)
     t.start()
     main()
